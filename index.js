3932d11a71c284c8c3b18a35cd080c6d02b14860
shan <shan@shan-Aspire-4749.(none)>
2014-04-07 22:37:58 +0800

    go to next and 404 if no next

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 var http = require("http");
 module.exports = express = function() {
 	var app = function(req, res, next) {
-		app.stack[0](req, res, next);
+		app.handle(req, res, next);
 	}
 	app.listen = function(port, callback) {
 		return http.createServer(this).listen(port, callback);
 	}
 
 	app.stack = [];
+
+	app.handle = function(req, res, next) {
+		var index = 0, stack = this.stack;
+		function next() {
+			var layer = stack[index++];
+			if (!layer) {
+				res.statusCode = 404;
+        			res.end("404 - Not Found");
+			}else{
+				layer(req, res, next);
+				next();
+			}
+		}	
+		next();
+	}
+
 	app.use = function(m) {
 		this.stack .push(m);
 	}
+
 	return app;
  }
 
--- test/app_spec.js
+++ test/app_spec.js
@@ -5,34 +5,64 @@ var request = require("supertest"),
 var express = require("../");
 
 describe("Implement app.use",function() {
-  var app;
+	var app;
 
-  var m1 = function() {};
-  var m2 = function() {};
+	var m1 = function() {};
+	var m2 = function() {};
 
-  before(function() {
-    app = express();
-  });
+	before(function() {
+	  app = express();
+	});
 
-  it("should be able to add middlewares to stack",function() {
-    app.use(m1)
-    app.use(m2)
-    expect(app.stack).to.deep.equal([m1,m2]);
-  });
+	it("should be able to add middlewares to stack",function() {
+	  app.use(m1)
+	  app.use(m2)
+	  expect(app.stack).to.deep.equal([m1,m2]);
+	});
 });
 
 describe("Implement calling the middlewares",function() {
-  var app;
-  beforeEach(function() {
-    app = express();
-  });
+	var app;
+	beforeEach(function() {
+		app = express();
+	});
 
-  it("Should be able to call a single middleware",function(done) {
-    var m1 = function(req,res,next) {
-      res.end("hello from m1");
-    };
-    app.use(m1);
-    request(app).get("/").expect("hello from m1").end(done);
-  });
+	it("Should be able to call a single middleware",function(done) {
+		var m1 = function(req,res,next) {
+			res.end("hello from m1");
+		};
+		app.use(m1);
+		request(app).get("/").expect("hello from m1").end(done);
+	});
+
+	it("Should be able to call `next` to go to the next middleware",function(done) {
+	var m1 = function(req,res,next) {
+	  	next();
+	};
+	var m2 = function(req,res,next) {
+	 	res.end("hello from m2");
+	};
+	app.use(m1);
+	app.use(m2);
+	request(app).get("/").expect("hello from m2").end(done);
+	});
+
+	it("Should 404 at the end of middleware chain",function(done) {
+	  	var m1 = function(req,res,next) {
+	    		next();
+	  	};
+
+	  	var m2 = function(req,res,next) {
+	    		next();
+	  	};
+
+	  	app.use(m1);
+	  	app.use(m2);
+	  	request(app).get("/").expect(404).end(done);
+	});
+
+	it("Should 404 if no middleware is added",function(done) {
+	  	request(app).get("/").expect(404).end(done);
+	});
 
 });
